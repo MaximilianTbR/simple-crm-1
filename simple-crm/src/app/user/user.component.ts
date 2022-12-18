2152ea93dcc692f4a0b0c8a744087a583af5aa64
Maximilian Tauber <maximilian.tauber@icloud.com>
2022-12-18 21:43:44 +0100

    added ngFor function in user component to show users from database

--- simple-crm/src/app/user/user.component.ts
+++ simple-crm/src/app/user/user.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Firestore } from 'firebase/firestore';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from '../models/user.class';
 @Component({
@@ -10,12 +12,20 @@ import { User } from '../models/user.class';
 export class UserComponent implements OnInit {
 
   user = new User();
+  allUsers = [];
 
-  constructor(public dialog: MatDialog) {
+  constructor(public dialog: MatDialog, private firestore: AngularFirestore) {
 
   }
 
   ngOnInit(): void {
+    this.firestore
+      .collection('users')
+      .valueChanges()
+      .subscribe((changes: any) => {
+        this.allUsers = changes;
+        console.log(this.allUsers);
+      })
   }
 
   openDialog() {
